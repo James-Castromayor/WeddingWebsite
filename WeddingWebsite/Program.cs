01be83d2d89d8e0e5b9efbcdbc6690719e225a26
James Castromayor <jamescastromayor@deltek.com>
2025-09-20 06:26:15 +0800

    Pilot run

--- WeddingWebsite/Program.cs
+++ WeddingWebsite/Program.cs
@@ -1,19 +1,11 @@
 using WeddingWebsite.Components;
+using WeddingWebsite.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-builder.Services.AddServerSideBlazor();
-
-// Add session support for simple state management (no database yet)
-builder.Services.AddSession(options =>
-{
-    options.IdleTimeout = TimeSpan.FromMinutes(30);
-    options.Cookie.HttpOnly = true;
-    options.Cookie.IsEssential = true;
-});
 
 // Add custom services
 builder.Services.AddScoped<ChatService>();
@@ -30,18 +22,9 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-app.UseRouting();
-app.UseSession();
-
 app.UseAntiforgery();
 
-app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
-
-app.MapRazorPages();
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
-
 
 app.Run();
